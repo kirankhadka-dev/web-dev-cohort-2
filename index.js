977fca33ac9b6c88b5b22da6bfb55e66aee422d6
Kiran Khadka <kiran.khadka.dev@gmail.com>
2026-03-25 14:27:53 +1100

    Merge feat/a

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function add(a,b){
+    if(!(a > b)){
+        throw new Error("number a must be greater than number b ")
+    }
     return a+b
 }
 
+
+
+function sub(a,b){
+    return a-b
+}
 add(2,5)
 add(10,23)
 add(34,45)
+sub(10,12)
+
+
